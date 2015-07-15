ff2856abac7f0a82f7411c74164dafc85fbcbbad
TheBiblMan <GenerallyCool@hotmail.com>
2015-07-16 00:32:45 +0100

    Mavenised wow. -Bibl

---
+++ org.nullbool.pi.core.engine.oldschoolimpl/src/main/java/org/nullbool/pi/core/engine/impl/factory/ext/Builder.java
@@ -0,0 +1,80 @@
+package org.nullbool.pi.core.engine.impl.factory.ext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Builder {
+	private final int id;
+	private String relativeName;
+	private int priority;
+	private boolean runnable;
+	private boolean optional;
+	private List<Integer> require;
+	private IActor<?> actor;
+	
+	public Builder(int id) {
+		this.id = id;
+		priority = -1;
+		require = new ArrayList<Integer>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getRelativeName() {
+		return relativeName;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+
+	public boolean isRunnable() {
+		return runnable;
+	}
+
+	public List<Integer> getRequire() {
+		return require;
+	}
+	
+	public IActor<?> getActor() {
+		return actor;
+	}
+	
+	public Builder relativeName(String relativeName) {
+		this.relativeName = relativeName;
+		return this;
+	}
+
+	public Builder priority(int priority) {
+		this.priority = priority;
+		return this;
+	}
+	
+	public Builder runnable(boolean b) {
+		runnable = b;
+		return this;
+	}
+	
+	public Builder require(int... ids) {
+		for(int i : ids) {
+			require.add(i);
+		}
+		return this;
+	}
+	
+	public Builder actor(IActor<?> actor) {
+		this.actor = actor;
+		return this;
+	}
+	
+	public Builder optional(boolean optional) {
+		this.optional = optional;
+		return this;
+	}
+	
+	public boolean isOptional() {
+		return optional;
+	}
+}
